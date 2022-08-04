82c68fa33d2c666998e75d6c2f906fde0b48f2b7
Zach <shaw.zacht@gmail.com>
2022-08-04 14:46:03 -0700

    commit tree 76d732ddc4a5b1718e3c8edd0fee150246096a59

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,22 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(string)
+
+    stack = Stack.new
+    @open = []
+    @closed = []
+    @additional = 0
+    string.chars.each do |char|
+        if char == ")" 
+            @open.push(char)
+        else
+            @closed.push(char)
+        end
+    end
+        if string.chars[0] == ")" && string.chars[string.chars.length - 1] == "("
+            @additional = 2
+        else nil
+        end
+    val = @open.size - @closed.size
+    val.abs + @additional
+end
